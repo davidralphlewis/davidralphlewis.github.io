eb632e71b5145f31f210ea2c398bcf6c0d5143f9
davidralphlewis <65033318+davidralphlewis@users.noreply.github.com>
2021-01-10 14:51:57 +0000

    better cookies

---
+++ _includes/cookies.js
@@ -0,0 +1,19 @@
+<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "content": {
+    "message": "This website uses cookies.  By continuing to use the website you agree to accept them.",
+    "dismiss": "Got it!",
+    "deny": "No ",
+    "link": "Learn More"
+  }
+});
+</script>
