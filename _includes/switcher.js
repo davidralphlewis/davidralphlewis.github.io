40961450c219814142b94ae8599ceaaa360cd37a
davidralphlewis <davidlewis887@gmail.com>
2021-11-10 13:12:33 +0000

    Update switcher.js

--- _includes/switcher.js
+++ _includes/switcher.js
@@ -3,6 +3,7 @@
 // Select the button
 const btn = document.querySelector('.btn-toggle');
 
+
 // Listen for a click on the button
 btn.addEventListener('click', function() {
   // Then toggle (add/remove) the .dark-theme class to the body
