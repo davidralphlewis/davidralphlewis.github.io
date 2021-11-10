8c878b8344f2df3c0e3a945f1dd667c2fdb796db
davidralphlewis <davidlewis887@gmail.com>
2021-11-10 10:27:50 +0000

    dark theme fixes

--- _includes/switcher.js
+++ _includes/switcher.js
@@ -8,3 +8,27 @@ btn.addEventListener('click', function() {
   // Then toggle (add/remove) the .dark-theme class to the body
   document.body.classList.toggle('dark-theme');  
 })
+
+// If the current theme in localStorage is "dark"...
+if (currentTheme == "dark") {
+  // ...then use the .dark-theme class
+  document.body.classList.add("dark-theme");
+}
+ 
+// Listen for a click on the button 
+btn.addEventListener("click", function() {
+  // Toggle the .dark-theme class on each click
+  document.body.classList.toggle("dark-theme");
+  
+  // Let's say the theme is equal to light
+  let theme = "light";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("dark-theme")) {
+    // ...then let's make the theme dark
+      theme = "dark";
+
+  }
+
+      // Then save the choice in localStorage
+  localStorage.setItem("theme", theme);
+});
