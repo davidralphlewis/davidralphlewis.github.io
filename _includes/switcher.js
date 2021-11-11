01590cfe6aa2743a2702847b3d9c22df85c6d891
davidralphlewis <davidlewis887@gmail.com>
2021-11-11 21:25:11 +0000

    Update switcher.js

--- _includes/switcher.js
+++ _includes/switcher.js
@@ -1,35 +1,18 @@
-
-
 // Select the button
-const btn = document.querySelector('.btn-toggle');
-
+const btn = document.querySelector(".btn-toggle");
 
 // Listen for a click on the button
-btn.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('dark-theme');  
-})
-
-// If the current theme in localStorage is "dark"...
-if (currentTheme == "dark") {
-  // ...then use the .dark-theme class
-  document.body.classList.add("dark-theme");
-}
- 
-// Listen for a click on the button 
 btn.addEventListener("click", function() {
-  // Toggle the .dark-theme class on each click
+  // Toggle the .dark-theme class on the body
   document.body.classList.toggle("dark-theme");
-  
+
   // Let's say the theme is equal to light
   let theme = "light";
   // If the body contains the .dark-theme class...
   if (document.body.classList.contains("dark-theme")) {
     // ...then let's make the theme dark
-      theme = "dark";
-
+    theme = "dark";
   }
-
-      // Then save the choice in localStorage
-  localStorage.setItem("theme", theme);
+  // Then save the choice in a cookie
+  document.cookie = "theme=" + theme;
 });
