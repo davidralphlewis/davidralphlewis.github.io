c1235ab827e942f7aab60a40aafeb492c0d1b335
davidralphlewis <davidlewis887@gmail.com>
2024-07-01 20:09:07 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "David Ralph Lewis",
+	url: "https://www.davidralphlewis.co.uk",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Poet and writer",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "David Ralph Lewis",
+		email: "contact@davidralphlewis.co.uk",
+		url: "https://www.davidralphlewis.co.uk"
 	}
 }
